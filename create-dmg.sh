ab23efbdc873ef5da76091422641c9822b882854
Shubham Panchal <shubhampanchal@google.com>
2025-07-07 12:31:11 +0530

    refactor scripts

---
+++ create-dmg.sh
@@ -0,0 +1,7 @@
+create-dmg \
+  --volname "Android File Browser" \
+  --window-size 500 300 \
+  --icon "AndroidFileBrowser.app" 130 100 \
+  --app-drop-link 370 100 \
+  bin/AndroidFileBrowser.dmg \
+  dist/AndroidFileBrowser.app
